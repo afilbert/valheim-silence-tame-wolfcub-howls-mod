4f673c8a54fc183b26bfe3d624ac1c54e7fdfe20
Aron Filbert <afilbert@lyconic.com>
2022-12-17 14:29:57 -0600

    Completed mod
    Inspired by Keezys_Better_Wolves mod, this riffs slightly differently in that it will only look for young tame wolves within 30 meters of the player. If any are found, then all wolf howls are prevented from triggering. This means that you can be accompanied by an adult tame wolf or wolves while hunting in the mountains, and still be alerted to enemy wolves by their howls when they spawn; while being able to enjoy peace and quiet at home

---
+++ ValheimTameHowlMod/ValheimTameHowlMod.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace TameMods
+{
+    [BepInPlugin("afilbert.ValheimTameHowlMod", "Valheim - Silence Tame Wolf Cub Howls", "0.0.1")]
+    [BepInProcess("valheim.exe")]
+    public class ValheimTameHowlMod : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("afilbert.ValheimTameHowlMod");
+
+        void Awake()
+        {
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(ZSFX), nameof(ZSFX.Play))]
+        class SilenceTheirYoungPatch
+        {
+            static bool Prefix(ref ZSFX __instance)
+            {
+                bool allowAttachment = true;
+                if (__instance.name.Contains("sfx_wolf_haul"))
+                {
+                    System.Collections.Generic.List<Character> characters = new System.Collections.Generic.List<Character>();
+                    Character.GetCharactersInRange(__instance.transform.position, 30f, characters);
+                    allowAttachment = !characters.Exists(c => c.IsTamed() && (c.name == "Wolf_cub(Clone)" || c.name == "Wolf_cub"));
+                }
+                return allowAttachment;
+            }
+        }
+    }
+}
