f79287ca64af035f49f609f7bbbf5f2b47b65f2e
Aron Filbert <afilbert@lyconic.com>
2024-12-23 16:58:49 -0600

    feat: Support silencing chickens
    chore: Update assemblies

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Valheim - Silence Tame Wolf Cub Howls Mod
 
-**_Works with Mistlands Update!_**
+**_Works with Ashlands Update!_**
 
 This is inspired by [Keezys_Better_Wolves mod](https://github.com/marqgray/valheim-better-wolves), and tweaks the approach to allow hunting with tamed wolves without the loss of enemy wolf howls.
 
@@ -17,12 +17,17 @@ This is inspired by [Keezys_Better_Wolves mod](https://github.com/marqgray/valhe
 
 While tame wolves won't attack one of their own, they're handy companions in most exploring/hunting situations. So having them present shouldn't also silence enemy wolves.
 
+### Chickens
+
+Chickens hadn't yet been introduced to the game when this mod was first developed. After cultivating a large and well-fed chicken population, they started strutting their stuff and making more and more love. Every time chickens make love (and maybe when laying eggs), an `sfx_love` audio clip plays that's the sound of a chicken crowing. That got annoying, quickly. So I build in support to turn it off. Optionally enjoy!
+
 ### Config
 
 Configuration now allows:
 
 * EnableToggleFlag, default: true
 * SilenceAdultsFlag, default: false
+* SilenceChickens, default: false
 * SilenceRangeValue, default: 30f
 
 Built with [BepInEx](https://valheim.thunderstore.io/package/denikson/BepInExPack_Valheim/)
@@ -35,6 +40,7 @@ Built with [BepInEx](https://valheim.thunderstore.io/package/denikson/BepInExPac
 
 Releases in github repo are packaged for Thunderstore Mod Manager.
 
+* 0.1.0 Add config to silence chicken crowing
 * 0.0.7 Drop requirement for [Official BepInEx ConfigurationManager](https://valheim.thunderstore.io/package/Azumatt/Official_BepInEx_ConfigurationManager/) from Thunderstore manifest.json to allow flexibility
 * 0.0.6 Target latest BepInEx version 5.4.2105
 * 0.0.5 Update for latest version of BepInEx
--- SilenceTameWolfCubMod.cs
+++ SilenceTameWolfCubMod.cs
@@ -1,24 +1,30 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
 using BepInEx.Configuration;
 using HarmonyLib;
+using System.ComponentModel;
 
 namespace ValheimTameMods
 {
-    [BepInPlugin("afilbert.ValheimSilenceTameWolfCubMod", "Valheim - Silence Tame Wolf Cub Howls", "0.0.7")]
+    [BepInPlugin("afilbert.ValheimSilenceTameWolfCubMod", "Valheim - Silence Tame Wolf Cub Howls", "0.1.0")]
     [BepInProcess("valheim.exe")]
     public class SilenceTameWolfCubMod : BaseUnityPlugin
     {
         private readonly Harmony harmony = new Harmony("afilbert.ValheimSilenceTameWolfCubMod");
+        public static ManualLogSource logger;
 
         public static ConfigEntry<bool> EnableToggle;
         public static ConfigEntry<bool> SilenceAdults;
+        public static ConfigEntry<bool> SilenceChickens;
         public static ConfigEntry<float> SilenceRange;
 
         void Awake()
         {
             harmony.PatchAll();
+            logger = Logger;
             EnableToggle = Config.Bind<bool>("Mod", "EnableToggleFlag", true, "Enable this mod");
             SilenceAdults = Config.Bind<bool>("Mod", "SilenceAdultsFlag", false, "Also silence adult wolves (tamed or otherwise)");
+            SilenceChickens = Config.Bind<bool>("Mod", "SilenceChickens", false, "Also silence chicken crowing");
             SilenceRange = Config.Bind<float>("Range", "SilenceRangeValue", 30f, "Radius in meters within which wolves are silenced");
         }
 
@@ -27,13 +33,25 @@ namespace ValheimTameMods
         {
             static bool Prefix(ref ZSFX __instance)
             {
+                if (!EnableToggle.Value)
+                {
+                    return true;
+                }
+
                 bool allowAttachment = true;
-                if (__instance.name.Contains("sfx_wolf_haul") && EnableToggle.Value)
+
+                if (__instance.name.Contains("sfx_wolf_haul"))
                 {
                     System.Collections.Generic.List<Character> characters = new System.Collections.Generic.List<Character>();
                     Character.GetCharactersInRange(__instance.transform.position, SilenceRange.Value, characters);
                     allowAttachment = !characters.Exists(c => (c.IsTamed() || SilenceAdults.Value) && (c.name == "Wolf_cub(Clone)" || c.name == "Wolf_cub" || (SilenceAdults.Value && (c.name == "Wolf(Clone)" || c.name == "Wolf"))));
                 }
+
+                if (SilenceChickens.Value && __instance.name.Contains("sfx_love"))
+                {
+                    allowAttachment = false;
+                }
+
                 return allowAttachment;
             }
         }
